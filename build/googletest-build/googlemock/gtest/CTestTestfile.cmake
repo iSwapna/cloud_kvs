813f3c96fd3f5969ead833cc9d18956142cd7431
Swapna <swapna@Admins-MacBook-Pro.local>
2017-06-28 23:26:06 -0700

    Delete build dir

--- build/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/swapna/playground/t_mp1/build/googletest-src/googletest
-# Build directory: /Users/swapna/playground/t_mp1/build/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
